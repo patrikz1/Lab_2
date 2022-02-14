929f82b6c0bb27d1f824a1a1b83de2950e57b216
patrikz1 <43486073+patrikz1@users.noreply.github.com>
2022-02-14 17:55:57 +0100

    Add files via upload

---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/* using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+} 
+
+Detta över är den "gamla" instansieringen av main klassen, det andra var bara det vi ser över,
+kanske är bäst å använda denna för är bara denna jag använt förut, Idno..?
+*/ 
---
+++ Lab 2/Lab 2/Shapes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    internal class Shapes 
+    {
+        //Gör Circles, Squares, Triangles och de likadana attributen, skrivs här å gör så att de inherit denna class.
+        // sen gör jag en lista av shapes här, å skickar det till en csv fil.
+    }
+}
