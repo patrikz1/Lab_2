6eb652b48e3ccefbba4748f0503206b66fe09bb2
mseltsegai <msely_1@hotmail.com>
2022-02-19 19:55:54 +0100

    areamethod

--- Lab 2/Lab 2/Circles.cs
+++ Lab 2/Lab 2/Circles.cs
@@ -8,11 +8,27 @@ namespace Lab_2
 {
     internal class Circles
     {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int length { get; set; }
 
-        public Circles()
+            static void areaCircle(int length)
+            {
+                Console.Write("Enter Length: ");
+                double l = Convert.ToDouble(Console.ReadLine());
+                double Area = Math.PI * length * length;
+                Console.WriteLine("Area of circle: " + Area);
+                Console.ReadKey();
+            }
+
+        public void calculateCircumference(int length)
         {
-
+            return 2 * Math.PI * radius;
         }
+
+        console.log(Math.PI);
         
     }
+        
+    
 }
